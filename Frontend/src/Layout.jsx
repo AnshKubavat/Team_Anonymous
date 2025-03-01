16746e738f6f9069b7e91264706d484c6f2d978f
Ansh <anshkubavat@gmail.com>
2025-03-01 12:03:51 +0530

    Layout changed

--- Frontend/src/Layout.jsx
+++ Frontend/src/Layout.jsx
@@ -3,13 +3,11 @@ import Login from "./Pages/Login/Login";
 import Signup from "./Pages/Signup/Signup";
 import Navbar from "./components/Navbar";
 
-export default function Layout() {
-  
 
 const Layout = () => {
   return (
     <>
-      
+
       <Routes>
         <Route path="/" element={<Navbar />} />
         <Route path="/login" element={<Login />} />
