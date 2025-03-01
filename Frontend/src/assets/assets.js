4ac37315728fdef7977c8f3e34c863db53887ce1
Nevil-Nakrani <nakaraninevil2017@gmail.com>
2025-03-01 11:51:12 +0530

    Assets changed

--- Frontend/src/assets/assets.js
+++ Frontend/src/assets/assets.js
@@ -1,5 +1,9 @@
 import logo from "./nearbygo_logo.webp";
 import userlogo from "./userlogo.jpg";
+import loginImg from "./login.png";
+import signupLogo from "./signup.png";
+import Slider_1 from "./Slider_1.png";
+import store from "store.png";
 
 const cities = [
   "Anand",
@@ -28,4 +32,14 @@ const categories = [
 ];
 
 const languages = ["English", "Gujarati", "Hindi"];
-export { logo, userlogo, cities, categories, languages };
+export {
+  logo,
+  userlogo,
+  cities,
+  categories,
+  languages,
+  loginImg,
+  signupLogo,
+  Slider_1,
+  store,
+};
