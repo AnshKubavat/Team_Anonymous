742a04c73b9157807b948bc06fc48fb492231a35
use <sathvaraneel52@gmail.com>
2025-03-02 03:51:12 +0530

    some styling in admin page

--- Frontend/src/Pages/AdminDashboard/components/AllBusinesses.jsx
+++ Frontend/src/Pages/AdminDashboard/components/AllBusinesses.jsx
@@ -1,10 +1,10 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { motion } from "framer-motion";
 import { MapPin, ChevronDown, Tag } from "lucide-react";
 import axiosClient from "../../../utils/axiosClient";
 import { toast } from "react-toastify";
 import ReactPaginate from "react-paginate";
-
+import { cities, categories } from "../../../assets/assets";
 const AllBusinesses = () => {
   const [cityDropdown, setCityDropdown] = useState(false);
   const [categoryDropdown, setCategoryDropdown] = useState(false);
@@ -15,6 +15,7 @@ const AllBusinesses = () => {
   const [businesses, setBusinesses] = useState([]);
   const [selectedSeller, setSelectedSeller] = useState(null);
   const [currentPage, setCurrentPage] = useState(0);
+  const dropdownRef = useRef(null);
   const itemsPerPage = 7;
 
   const handleSellerClick = (seller) => {
@@ -37,32 +38,20 @@ const AllBusinesses = () => {
   useEffect(() => {
     fetchAllBusinesses();
   }, []);
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        setCityDropdown(false);
+        setCategoryDropdown(false);
+      }
+    };
 
-  const cities = [
-    "Rajkot",
-    "Vallabh Vidyanagar",
-    "Vadodara",
-    "Anand",
-    "Ahmedabad",
-  ];
-  const categories = [
-    "All",
-    "Restaurants",
-    "Stationary Shop",
-    "Cobbler",
-    "Milkman",
-    "Carpenter",
-    "Blacksmith",
-    "Flower Shop",
-    "Laundry",
-    "Electrician",
-    "Plumber",
-    "Salon & Barber",
-    "Grocery Store",
-    "Bakery",
-    "Tailor",
-    "Mechanic",
-  ];
+    document.addEventListener("click", handleClickOutside);
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, []);
+
   const filteredCities = cities.filter((city) =>
     city.toLowerCase().includes(citySearch.toLowerCase())
   );
@@ -71,21 +60,30 @@ const AllBusinesses = () => {
   );
 
   const filteredSellers = businesses.filter((business) => {
-    return (
-      (selectedCity === "" || business.city === selectedCity) &&
-      (selectedCategory === "All" ||
-        business.categoryOfBusiness === selectedCategory.toLocaleLowerCase())
-    );
+    const cityMatch = selectedCity === "" || business.city === selectedCity;
+    const categoryMatch =
+      selectedCategory === "All" ||
+      business.categoryOfBusiness.toLowerCase() ===
+        selectedCategory.toLowerCase();
+    return cityMatch && categoryMatch;
   });
+
   const handlePageChange = ({ selected }) => {
     setCurrentPage(selected);
+  };
+  const handleCityDropdownClick = () => {
+    setCityDropdown(!cityDropdown);
+    setCategoryDropdown(false);
+  };
+  const handleCategoryDropdownClick = () => {
+    setCategoryDropdown(!categoryDropdown);
+    setCityDropdown(false);
   };
 
   const displayedSellers = filteredSellers.slice(
     currentPage * itemsPerPage,
     (currentPage + 1) * itemsPerPage
   );
-
 
   const handleDelete = async (id) => {
     try {
@@ -103,19 +101,22 @@ const AllBusinesses = () => {
   return (
     <div className="min-h-[800px] overflow-y-auto bg-gray-100 p-6  flex flex-col gap-6">
       {/* Filter Section */}
-      <div className="flex flex-col md:flex-row mx-auto lg:mx-0 mt-10 md:mt-0 gap-4 mb-2">
+      <div
+        ref={dropdownRef}
+        className="flex flex-col md:flex-row mx-auto lg:mx-0 mt-10 md:mt-0 gap-4 mb-2"
+      >
         {/* City Dropdown with Search */}
         <div className="relative">
           <button
-            className="flex items-center text-gray-700 font-medium px-4 py-2 border border-gray-300 rounded-lg bg-white gap-2 w-80 "
-            onClick={() => setCityDropdown(!cityDropdown)}
+            className="flex items-center text-gray-700 font-medium px-4 py-2 border border-gray-300 rounded-lg bg-white gap-2 w-70 lg:w-80"
+            onClick={handleCityDropdownClick}
           >
             <MapPin size={18} className="text-black " />{" "}
             {selectedCity || "Select City"}
             <ChevronDown size={18} className="absolute right-3" />
           </button>
           {cityDropdown && (
-            <div className="absolute bg-white shadow-md rounded-lg  lg:w-98 mt-2 w-72 p-2 z-10 h-64 overflow-y-auto">
+            <div className="absolute bg-white shadow-md rounded-lg  lg:w-80 mt-2 w-70 p-2 z-10 h-64 overflow-y-auto">
               <input
                 type="text"
                 placeholder="Search city..."
@@ -146,15 +147,15 @@ const AllBusinesses = () => {
         {/* Category Dropdown with Search */}
         <div className="relative">
           <button
-            className="flex items-center text-gray-700 font-medium px-4 py-2 border border-gray-300 rounded-lg bg-white gap-2  w-72 lg:w-80"
-            onClick={() => setCategoryDropdown(!categoryDropdown)}
+            className="flex items-center text-gray-700 font-medium px-4 py-2 border border-gray-300 rounded-lg bg-white gap-2  w-70 lg:w-80"
+            onClick={handleCategoryDropdownClick}
           >
             <Tag size={18} className="text-black" />{" "}
             {selectedCategory || "Select Category"}
             <ChevronDown size={18} className="absolute right-3" />
           </button>
           {categoryDropdown && (
-            <div className="absolute bg-white shadow-md rounded-lg mt-2  lg:w-98  w-72 p-2 z-10 h-64 overflow-y-auto">
+            <div className="absolute bg-white shadow-md rounded-lg mt-2  lg:w-80  w-70 p-2 z-10 h-64 overflow-y-auto">
               <input
                 type="text"
                 placeholder="Search category..."
@@ -190,91 +191,91 @@ const AllBusinesses = () => {
 
       {/* Seller Details Table */}
       <div className=" bg-gray-100   flex flex-col gap-6">
-      <div className="bg-white shadow-lg p-6  overflow-x-auto  rounded-xl w-full">
-        <h2 className="text-xl font-bold mb-4">Seller Details</h2>
-        <table className="w-full border-collapse border border-gray-200">
-          <thead>
-            <tr className="bg-gray-200">
-              <th className="border p-2">Index</th>
-              <th className="border p-2">Seller Name</th>
-              <th className="border p-2">City</th>
-              <th className="border p-2">Shop Category</th>
-              <th className="border p-2">Detail</th>
-              <th className="border p-2">Email</th>
-              <th className="border p-2">Delete</th>
-            </tr>
-          </thead>
-          <tbody>
-            {displayedSellers.map((seller, index) => (
-              <motion.tr
-                key={index}
-                initial={{ opacity: 0, y: 10 }}
-                animate={{ opacity: 1, y: 0 }}
-                transition={{ duration: 0.3 }}
-                className="border hover:bg-gray-100"
-              >
-                <td className="border p-2 text-center">{index + 1}</td>
-                <td className="border p-2 text-center">
-                  {seller.sellerDetails?.username}
-                </td>
-                <td className="border p-2 text-center">{seller.city}</td>
-                <td className="border p-2 text-center">
-                  {seller.categoryOfBusiness}
-                </td>
-                <td className="border p-2 text-center">
-                  <motion.button
-                    whileHover={{ scale: 1.1 }}
-                    whileTap={{ scale: 0.9 }}
-                    onClick={() => handleSellerClick(seller)}
-                    className="bg-blue-500 text-white px-4 py-2 rounded"
-                  >
-                    Detail
-                  </motion.button>
-                </td>
-                <td className="border p-2 text-center">
-                  <motion.button
-                    whileHover={{ scale: 1.1 }}
-                    whileTap={{ scale: 0.9 }}
-                    onClick={() => {
-                      const email = seller.sellerDetails.email;
-                      if (email) {
-                        window.location.href = `https://mail.google.com/mail/?view=cm&to=${email}`;
-                      } else {
-                        toast.error("No email found!");
-                      }
-                    }}
-                    className="bg-green-500 text-white px-4 py-2 rounded"
-                  >
-                    Email
-                  </motion.button>
-                </td>
-                <td className="border p-2 text-center">
-                  <motion.button
-                    whileHover={{ scale: 1.1 }}
-                    whileTap={{ scale: 0.9 }}
-                    onClick={() => handleDelete(seller)}
-                    className="bg-red-500 text-white px-4 py-2 rounded"
-                  >
-                    Delete
-                  </motion.button>
-                </td>
-              </motion.tr>
-            ))}
-          </tbody>
-        </table>
-        <ReactPaginate
-          previousLabel={"Previous"}
-          nextLabel={"Next"}
-          pageCount={Math.ceil(filteredSellers.length / itemsPerPage)}
-          onPageChange={handlePageChange}
-          containerClassName="flex justify-center mt-4 cursor-pointer"
-          previousLinkClassName="bg-gray-300 px-3 py-1 rounded mr-2"
-          nextLinkClassName="bg-gray-300 px-3 py-1 rounded ml-2"
-          disabledClassName="opacity-50"
-          activeClassName="bg-blue-500 mx-2 text-white px-3  rounded"
-        />
+        <div className="bg-white shadow-lg p-6  overflow-x-auto  rounded-xl w-full">
+          <h2 className="text-xl font-bold mb-4">Seller Details</h2>
+          <table className="w-full border-collapse border border-gray-200">
+            <thead>
+              <tr className="bg-gray-200">
+                <th className="border p-2">Index</th>
+                <th className="border p-2">Seller Name</th>
+                <th className="border p-2">City</th>
+                <th className="border p-2">Shop Category</th>
+                <th className="border p-2">Detail</th>
+                <th className="border p-2">Email</th>
+                <th className="border p-2">Delete</th>
+              </tr>
+            </thead>
+            <tbody>
+              {displayedSellers.map((seller, index) => (
+                <motion.tr
+                  key={index}
+                  initial={{ opacity: 0, y: 10 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  transition={{ duration: 0.3 }}
+                  className="border hover:bg-gray-100"
+                >
+                  <td className="border p-2 text-center">{index + 1}</td>
+                  <td className="border p-2 text-center">
+                    {seller.sellerDetails?.username}
+                  </td>
+                  <td className="border p-2 text-center">{seller.city}</td>
+                  <td className="border p-2 text-center">
+                    {seller.categoryOfBusiness}
+                  </td>
+                  <td className="border p-2 text-center">
+                    <motion.button
+                      whileHover={{ scale: 1.1 }}
+                      whileTap={{ scale: 0.9 }}
+                      onClick={() => handleSellerClick(seller)}
+                      className="bg-blue-500 text-white px-4 py-2 rounded"
+                    >
+                      Detail
+                    </motion.button>
+                  </td>
+                  <td className="border p-2 text-center">
+                    <motion.button
+                      whileHover={{ scale: 1.1 }}
+                      whileTap={{ scale: 0.9 }}
+                      onClick={() => {
+                        const email = seller.sellerDetails.email;
+                        if (email) {
+                          window.location.href = `https://mail.google.com/mail/?view=cm&to=${email}`;
+                        } else {
+                          toast.error("No email found!");
+                        }
+                      }}
+                      className="bg-green-500 text-white px-4 py-2 rounded"
+                    >
+                      Email
+                    </motion.button>
+                  </td>
+                  <td className="border p-2 text-center">
+                    <motion.button
+                      whileHover={{ scale: 1.1 }}
+                      whileTap={{ scale: 0.9 }}
+                      onClick={() => handleDelete(seller)}
+                      className="bg-red-500 text-white px-4 py-2 rounded"
+                    >
+                      Delete
+                    </motion.button>
+                  </td>
+                </motion.tr>
+              ))}
+            </tbody>
+          </table>
+          <ReactPaginate
+            previousLabel={"Previous"}
+            nextLabel={"Next"}
+            pageCount={Math.ceil(filteredSellers.length / itemsPerPage)}
+            onPageChange={handlePageChange}
+            containerClassName="flex justify-center mt-4 cursor-pointer"
+            previousLinkClassName="bg-gray-300 px-3 py-1 rounded mr-2"
+            nextLinkClassName="bg-gray-300 px-3 py-1 rounded ml-2"
+            disabledClassName="opacity-50"
+            activeClassName="bg-blue-500 mx-2 text-white px-3  rounded"
+          />
+        </div>
       </div>
-    </div>
 
       {selectedSeller && (
         <>
@@ -319,16 +320,19 @@ const AllBusinesses = () => {
                 <strong>City:</strong> {selectedSeller.city}
               </p>
               <p className="text-gray-700">
-                <strong>Shop Category:</strong> {selectedSeller.categoryOfBusiness}
+                <strong>Shop Category:</strong>{" "}
+                {selectedSeller.categoryOfBusiness}
               </p>
               <p className="text-gray-700">
-                <strong>Email:</strong> {selectedSeller.sellerDetails.email || "N/A"}
+                <strong>Email:</strong>{" "}
+                {selectedSeller.sellerDetails.email || "N/A"}
               </p>
               <p className="text-gray-700">
                 <strong>Product Count:</strong> {selectedSeller.productCount}
               </p>
               <p className="text-gray-700">
-                <strong>is Active :</strong> {selectedSeller.isActive=== true ? "True" : "False"}
+                <strong>is Active :</strong>{" "}
+                {selectedSeller.isActive === true ? "True" : "False"}
               </p>
             </div>
           </motion.div>
