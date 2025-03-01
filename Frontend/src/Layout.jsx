548196b86c5388bf78f352b40e09010e20477e7e
Dev3483 <devmali.552005@gmail.com>
2025-03-01 13:17:35 +0530

    change in layout,add navbar and footer

--- Frontend/src/Layout.jsx
+++ Frontend/src/Layout.jsx
@@ -2,17 +2,24 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "./Pages/Login/Login";
 import Signup from "./Pages/Signup/Signup";
 import Navbar from "./components/Navbar";
+import Home from "./Pages/Home/Home";
+import Footer from "./components/Footer";
 
 
 const Layout = () => {
+   const hideNavbarFooter =
+    location.pathname === "/login" ||
+    location.pathname === "/signup";
   return (
     <>
+      {!hideNavbarFooter && <Navbar />}
 
       <Routes>
-        <Route path="/" element={<Navbar />} />
+        <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
       </Routes>
+      {!hideNavbarFooter && <Footer />}
     </>
   );
 }
