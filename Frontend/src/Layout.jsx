1068ae71fc3bacb4403d9b43e0c26432bc844d76
Ansh <anshkubavat@gmail.com>
2025-03-01 13:15:54 +0530

    add category frontend

--- Frontend/src/Layout.jsx
+++ Frontend/src/Layout.jsx
@@ -2,6 +2,7 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "./Pages/Login/Login";
 import Signup from "./Pages/Signup/Signup";
 import Navbar from "./components/Navbar";
+import Category from "./Pages/Category/Category";
 
 
 const Layout = () => {
@@ -10,6 +11,7 @@ const Layout = () => {
 
       <Routes>
         <Route path="/" element={<Navbar />} />
+        <Route path="/category" element={<Category />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
       </Routes>
